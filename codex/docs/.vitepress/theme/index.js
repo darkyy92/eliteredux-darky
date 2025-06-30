92898ad1d0b049bd0ba676d84a300061cabb7b15
Joël <joel@staub-it.ch>
2025-06-30 06:54:00 +0200

    UI changes

--- codex/docs/.vitepress/theme/index.js
+++ codex/docs/.vitepress/theme/index.js
@@ -2,19 +2,16 @@ import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
 import './custom.css'
 import InlineAbilityEditor from './components/InlineAbilityEditor.vue'
-import SidebarAutoScroll from './components/SidebarAutoScroll.vue'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     // Register global components
     app.component('InlineAbilityEditor', InlineAbilityEditor)
-    app.component('SidebarAutoScroll', SidebarAutoScroll)
   },
   Layout() {
     return h(DefaultTheme.Layout, null, {
-      'doc-before': () => h(InlineAbilityEditor),
-      'layout-bottom': () => h(SidebarAutoScroll)
+      'doc-before': () => h(InlineAbilityEditor)
     })
   }
 }
